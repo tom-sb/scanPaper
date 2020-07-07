75f6f427880aab1bf05cda81ac2f217a1639f8c8
TomVilla <fvillanuevanut@gmail.com>
2020-07-06 20:42:17 -0500

    update2

--- exam2.py
+++ exam2.py
@@ -1,6 +1,6 @@
 import cv2 as cv
 import numpy as np
-
+from fractions import Fraction
 from matplotlib import pyplot as plt
 
 class Image:
@@ -15,33 +15,33 @@ class Image:
 		imageGray = cv.cvtColor(self.image,cv.COLOR_BGR2GRAY)
 		return imageGray
 	
-	def makePadding(self, pixBorder, valPix = 0):
+	def makePadding(self, pixBorder):
 		self.beGray()
-		newMatrix = [[]for i in range(int(self.row + (pixBorder)))]
-		for i in range(self.row+(pixBorder)):
-			for j in range(self.col+(pixBorder)):
-				if(i == 0 or j == 0):
-					newMatrix[i].append(valPix)
-				elif(i == self.row+1 or j ==self.col+1):
-					newMatrix[i].append(valPix)
-				else:
-					newMatrix[i].append(self.image[i-1,j-1])
+		#newMatrix = [[]for i in range(int(self.row + (pixBorder)))]
+		newMatrix = np.zeros((self.row + pixBorder, self.col + pixBorder))
+		for i in range(int(pixBorder/2), self.row):
+			for j in range(int(pixBorder/2), self.col):
+				if(i < self.col + (pixBorder/2) or j < self.row + (pixBorder/2)):
+					newMatrix[i,j] = self.image[i-(int(pixBorder/2)), j-(int(pixBorder/2))]
 		return newMatrix
 	
 	def GaussianFunc(self, x, y):
-		num = np.exp(((x*x)+(y*y))/2*(1.8*1.8))
-		den = 2*np.pi*(1.8*1.8)
-		print(num)
+		sigma = 1
+		num = np.exp(-1*((x*x)+(y*y))/(2*(sigma*sigma)))
+		den = 2*np.pi*(sigma*sigma)
+		print(num/den)
+		return num/den
 
 	def makeKernel(self, kernelSize):
 		kernelMatrix = [[]for i in range(kernelSize)]
 		for i in range(kernelSize):
 			for j in range(kernelSize):
 				kernelMatrix[i].append(self.GaussianFunc(i,j))
+		print(kernelMatrix)
 		return kernelMatrix
 
-	def convolution(self, kernelSize, valPix):
-		self.makePadding(kernelSize-1,valPix)
+	def convolution(self, kernelSize):
+		self.makePadding(kernelSize-1)
 		self.makeKernel(kernelSize)
 
 		
@@ -62,4 +62,4 @@ img = cv.imread('img1.png')
 
 
 img1 = Image(img)
-img1.convolution(3,0)
+img1.convolution(5)
