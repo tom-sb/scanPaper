acb6791ed507cdac97e8d5f5b3a2345aa4024654
Jean Carlos soncco lupa <jsonccolu@unsa.edu.pe>
2020-07-08 11:45:45 -0500

    actualizar interactivo

--- figu.py
+++ figu.py
@@ -5,12 +5,31 @@ import numpy as np
 
 def formA(points):
     verts = [
-            (points[0,0], points[0,1]),   # P0
-            (points[1,0], points[1,1]),  # P1
-            (points[2,0], points[2,1]),  # P2
-            (points[3,0], points[3,1]),  # P3
-            (points[0,0], points[0,1]),
+            (points[0][0], points[0][1]),   # P0
+            (points[1][0], points[1][1]),  # P1
+            (points[2][0], points[2][1]),  # P2
+            (points[3][0], points[3][1]),  # P3
+            (points[0][0], points[0][1]),
             ]
     xs, ys = zip(*verts)
 
-    return xs,ys
+    return np.array(xs),np.array(ys)
+
+def distance(pt1,pt2):
+    suma=0
+    for i in range(2):
+        suma+=pow(pt2[i]-pt1[i],2)
+
+    return np.sqrt(suma)
+
+def take_second(elem):
+    return elem[1]
+
+def knn(puntos,punto):
+    vdis = []
+    c = 0
+    for p in puntos:
+        vdis.append((distance(p,punto),c))
+        c+=1
+    vdis.sort()
+    return vdis[0]
--- interactive1.py
+++ interactive1.py
@@ -1,91 +1,69 @@
 from exam2 import Image
-from figu import formA
+from figu import formA, knn
 import cv2 as cv
 import numpy as np
 import matplotlib.pyplot as plt
 
 class DraggableRectangle:
     lock = None  # only one can be animated at a time
-    def __init__(self, rect):
-        self.rect = rect
+    def __init__(self, scatter,line,_x,_y):
+        self.scatter = scatter
+        self.line=line
+        self.x =_x
+        self.y =_y
         self.press = None
         self.background = None
 
     def connect(self):
         'connect to all the events we need'
-        self.cidpress = self.rect.figure.canvas.mpl_connect(
+        self.cidpress = self.scatter.figure.canvas.mpl_connect(
             'button_press_event', self.on_press)
-        self.cidrelease = self.rect.figure.canvas.mpl_connect(
+        self.cidrelease = self.scatter.figure.canvas.mpl_connect(
             'button_release_event', self.on_release)
-        self.cidmotion = self.rect.figure.canvas.mpl_connect(
+        self.cidmotion = self.scatter.figure.canvas.mpl_connect(
             'motion_notify_event', self.on_motion)
 
     def on_press(self, event):
         'on button press we will see if the mouse is over us and store some data'
-        if event.inaxes != self.rect.axes: return
-        if DraggableRectangle.lock is not None: return
-        contains, attrd = self.rect.contains(event)
+        if event.inaxes != self.scatter.axes: return
+
+        contains, attrd = self.scatter.contains(event)
         if not contains: return
-        print('event contains', self.rect.xy)
-        x0, y0 = self.rect.xy
-        self.press = x0, y0, event.xdata, event.ydata
-        DraggableRectangle.lock = self
-
-        # draw everything but the selected rectangle and store the pixel buffer
-        canvas = self.rect.figure.canvas
-        axes = self.rect.axes
-        self.rect.set_animated(True)
-        canvas.draw()
-        self.background = canvas.copy_from_bbox(self.rect.axes.bbox)
-
-        # now redraw just the rectangle
-        axes.draw_artist(self.rect)
-
-        # and blit just the redrawn area
-        canvas.blit(axes.bbox)
+        #print('event contains', self.rect.xy)
+        #x0, y0 = self.rect.xy
+        #self.press = x0, y0, event.xdata, event.ydata
+        eventx, eventy = event.xdata, event.ydata
+        
+        pointd = knn(zip(self.x,self.y),(eventx,eventy))
+        self.x[pointd[1]] = eventx
+        self.y[pointd[1]] = eventy
 
     def on_motion(self, event):
         'on motion we will move the rect if the mouse is over us'
-        if DraggableRectangle.lock is not self:
-            return
-        if event.inaxes != self.rect.axes: return
-        x0, y0, xpress, ypress = self.press
-        dx = event.xdata - xpress
-        dy = event.ydata - ypress
-        self.rect.set_x(x0+dx)
-        self.rect.set_y(y0+dy)
+        if self.press is None: return
+        if event.inaxes != self.scatter.axes: return
+        #x0, y0, xpress, ypress = self.press
+        #dx = event.xdata - xpress
+        #dy = event.ydata - ypress
+        
+        #self.rect.set_x(x0+dx)
+        #self.rect.set_y(y0+dy)
 
-        canvas = self.rect.figure.canvas
-        axes = self.rect.axes
-        # restore the background region
-        canvas.restore_region(self.background)
+        self.scatter = plt.scatter(self.x,self.y,zorder=1)
+        self.line = plt.plot(self.x,self.y,zorder=1)
 
-        # redraw just the current rectangle
-        axes.draw_artist(self.rect)
-
-        # blit just the redrawn area
-        canvas.blit(axes.bbox)
+        self.scatter.figure.canvas.draw()
 
     def on_release(self, event):
         'on release we reset the press data'
-        if DraggableRectangle.lock is not self:
-            return
-
         self.press = None
-        DraggableRectangle.lock = None
-
-        # turn off the rect animation property and reset the background
-        self.rect.set_animated(False)
-        self.background = None
-
-        # redraw the full figure
-        self.rect.figure.canvas.draw()
+        self.scatter.figure.canvas.draw()
 
     def disconnect(self):
         'disconnect all the stored connection ids'
-        self.rect.figure.canvas.mpl_disconnect(self.cidpress)
-        self.rect.figure.canvas.mpl_disconnect(self.cidrelease)
-        self.rect.figure.canvas.mpl_disconnect(self.cidmotion)
+        self.scatter.figure.canvas.mpl_disconnect(self.cidpress)
+        self.scatter.figure.canvas.mpl_disconnect(self.cidrelease)
+        self.scatter.figure.canvas.mpl_disconnect(self.cidmotion)
 
 img = cv.imread("img1.png")
 orig = img.copy()
@@ -95,21 +73,14 @@ target = img1.findcontours(canny)
 approx = img1.mapper(target)
 
 final = img1.transform(approx,orig)
-#fig = plt.figure()
-#fig.figimage(img)
-#ax = fig.add_subplot(111)
-#rects = ax.bar(range(4), 20*np.random.rand(4))
 drs = []
 
 xs,ys=formA(np.array(approx))
 plt.imshow(img,zorder=0)
-plt.scatter(xs,ys,zorder=1)
-plt.plot(xs,ys,zorder=1)
-
-
-for point in approx:
-    dr = DraggableRectangle(point)
-    #dr.connect()
-    #drs.append(dr)
+scatter = plt.scatter(xs,ys,zorder=1)
+line, = plt.plot(xs,ys,zorder=1)
+dr = DraggableRectangle(scatter,line,xs,ys)
+dr.connect()
+drs.append(dr)
 
 plt.show()
